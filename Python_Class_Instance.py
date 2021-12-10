f7f97d8f7422bb8061e860f50c31a4ac975d62b5
MahmudHossain <bmahmudcse@gmail.com>
2021-12-11 00:32:06 +0600

    1st day python class program

---
+++ Python_Class_Instance.py
@@ -0,0 +1,18 @@
+class Employee:
+    def __init__(self,first,last) -> None:
+        self.first = first
+        self.last = last
+        self.email = first+'.'+last+'@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+
+emp1=Employee('mahmud','hossain')
+emp2=Employee('toufikur','rahman')
+
+print(emp1.email)
+print(emp2.email)
+print(emp1.fullname())
+print(emp2.fullname())
+#we need to explicitly define what on which object we want to apply this method
+print(Employee.fullname(emp2))
